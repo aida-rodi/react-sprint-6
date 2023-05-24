f7869926edd8789fd7d11eaa2cf06c3139aec126
aida-rodi <aida.rodi.1997@gmail.com>
2023-05-24 12:09:22 +0200

    exercise 1

---
+++ src/components/scene/scene.tsx
@@ -0,0 +1,4 @@
+// Aquest primer component només ha de retornar el següent string:
+//"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+
+export const scene = "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial.";
